1d3b46dbf8efae26b23c84bf6fb55c92fed6047a
Pratham <bhagatpratham101@gmail.com>
2023-03-12 15:26:15 +0530

    getting lyrics from api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,47 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [lyrics, setLyrics] = useState(null);
+  const [trackName, setTrackName] = useState("");
+  const [artistName, setArtistName] = useState("");
+  const [copied, setCopied] = useState(false);
+
+  const getTrack = (e) => {
+    setTrackName(e.target.value);
+  };
+
+  const getArtist = (e) => {
+    setArtistName(e.target.value);
+  };
+
+  const handleClick = async () => {
+    if (trackName.trim() === "") {
+      console.error("Track name can't be empty!");
+    } else {
+      const res = await fetch(
+        `https://lyrist.vercel.app/api/${trackName}/${artistName}`
+      );
+      if (res.ok) {
+        const data = await res.json();
+        setLyrics(data);
+      } else {
+        res.error("Lyrics not found!");
+      }
+    }
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard
+      .writeText(lyrics?.lyrics || "")
+      .then(() => {
+        setCopied(true);
+      })
+      .catch(() => {
+        console.error("Couldn't Copy!");
+      });
+  };
+
   return (
     <div className="App">
       <section className="logo">
@@ -11,15 +52,24 @@ function App() {
       </section>
       <section className="main-body">
         <div className="search-lyrics">
-          <input placeholder="Enter track name"></input>
+          <input placeholder="Enter track name" onChange={getTrack}></input>
           <div className="search-container">
-            <input placeholder="Enter artist name (optional)"></input>
-            <button className="search-btn">Search Lyrics</button>
+            <input
+              placeholder="Enter artist name (optional)"
+              onChange={getArtist}
+            ></input>
+            <button className="search-btn" onClick={handleClick}>
+              Search Lyrics
+            </button>
           </div>
         </div>
         <div className="lyrics">
-          <button className="copy-lyrics">Copy Lyrics</button>
-          <p className="display-lyrics">{"Nothing here yet"}</p>
+          <button className="copy-lyrics" onClick={handleCopy}>
+            Copy Lyrics
+          </button>
+          <p className="display-lyrics">
+            {lyrics?.lyrics || "Nothing here yet ..."}
+          </p>
         </div>
       </section>
     </div>
